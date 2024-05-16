d32a727c62aa04f316cf61e8914b0961a8f85365
hagansamuel6 <hagansamuel6@gmail.com>
2024-05-16 10:21:37 +0000

    arrays

--- array.js
+++ array.js
@@ -13,19 +13,65 @@ console.log(arrayTwo);
 let arrFour = new Array();
 arrFour[0] = "one";
 
-let arrFive = new Array(10, 20, 30); //with more than one arg, it becomes an array with those element
+//with more than one arg, it becomes an array with those element
+//with one arg, it becomes the size of the new array
+let arrFive = new Array(10, 20, 30);
 
 console.log(arrFive);
 
-
-//array with one element can have more than one element
+//array with just one element but can have more than one element
 let arrSix = Array.of(90);
-console.log('arrSix', arrSix);
+console.log("arrSix", arrSix);
 
 let obj = {
-    item1: "one",
-    item2: "two"
-}
+  item1: "one",
+  item2: "two",
+};
 
 let arrFrom = Array.from(obj);
-console.log("array from array like object",arrFrom); //obj not arraylike
+console.log("array from array like object", arrFrom); //obj not arraylike
+
+//check for an index existing in the array
+let arrCheck = 0 in arrSix; //index zero in array
+
+//check the length of an array
+let arrLength = arrSix.length;
+
+//adding elements to the end of an array
+arrSix.push("one", "two");
+
+//remove the last element and return it
+arrSix.pop();
+
+//adding elements to the start of the array
+arrSix.unshift("startOne", "startTwo");
+
+//remove the first element and return it.
+arrSix.shift();
+
+//delete an element from the array
+delete arrSix[0]; // no element at index 0 now, the array becomes sparse
+
+//the splice method,
+
+//use for of, to loop through an array
+
+for (let item of arrSix) {
+  //item is each item in the loop
+}
+
+//to get the indexes
+for (let [index, item] of arrSix.entries()) {
+  //index and item
+  item += item + " 1";
+}
+
+//we can use the foreach loop as well
+let arrRes = new Array();
+
+//for sparse arrays it does not invoke the element, 
+arrSix.forEach((item) => {
+  arrRes.unshift(item + "1");
+});
+
+console.log(arrRes);
